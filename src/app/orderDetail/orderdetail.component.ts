7a7c65fabcb11bde88f0c88e191f8575f0d23b85
ismail1986 <ismailece2007@gmail.com>
2017-12-17 00:03:09 +0530

    order details page added

---
+++ src/app/orderDetail/orderdetail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+export class OrderDetail implements OnInit {
+
+    constructor(private router: Router){}
+
+    ngOnInit(){
+
+    }
+}
